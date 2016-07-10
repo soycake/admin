fe8ea9c77fbe52d87eab3534285c2765396e35e1
Goragod <admin@goragod.com>
2016-07-10 12:05:17 +0700

    init git

---
+++ settings/database.php
@@ -0,0 +1,14 @@
+<?php
+/* settings/database.php */
+return array(
+  'mysql' => array(
+    'dbdriver' => 'mysql',
+    'username' => 'root',
+    'password' => '',
+    'dbname' => 'kotchasan',
+    'prefix' => 'kotchasan',
+  ),
+  'tables' => array(
+    'user' => 'user'
+  )
+);
